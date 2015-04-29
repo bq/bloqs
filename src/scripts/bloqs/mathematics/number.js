158de0c686b77b83976239cdc52afe9e86a193dd
Irene Sanz <irene.sanz@bq.com>
2015-04-29 15:48:01 +0200

    First working version of getCode() function for all bloqs. Oscillator and number bloqs fully working
    Change-Id: I275b7898d793ded05157d909c66aaae8cd55d0d8

--- src/scripts/bloqs/mathematics/number.js
+++ src/scripts/bloqs/mathematics/number.js
@@ -5,18 +5,15 @@ var _ = require('lodash');
 var OutputBloq = require('./../outputBloq');
 
 var bloq = _.merge(_.clone(OutputBloq, true), {
-
     name: 'number',
     content: [
         [{
+            id:'VALUE',
             alias: 'numberInput',
             value: 0
         }]
     ],
-    code: {
-        setup: ['{0}'],
-        loop: ['{0}']
-    }
+    code: '{VALUE}'
 });
 
 module.exports = bloq;
