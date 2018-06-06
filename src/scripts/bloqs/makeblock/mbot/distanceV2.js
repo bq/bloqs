13aa1fd9658c71cc218bce8057a86dcacc652ad1
Alberto Valero <alberto.valero@bq.com>
2018-06-06 17:40:22 +0200

    added mblock us read

--- src/scripts/bloqs/makeblock/mbot/distanceV2.js
+++ src/scripts/bloqs/makeblock/mbot/distanceV2.js
@@ -1,9 +1,9 @@
-/*global require */
-'use strict';
+/* global require */
 
-var _ = require('lodash'),
-    utils = require('./../../build-utils'),
-    OutputBloq = require('./../../outputBloq');
+
+let _ = require('lodash'),
+  utils = require('./../../build-utils'),
+  OutputBloq = require('./../../outputBloq');
 
 /**
  * Bloq name: mBotGetDistance
@@ -15,47 +15,47 @@ var _ = require('lodash'),
  * Return type: float
  */
 
-var mBotGetDistance = _.merge(_.clone(OutputBloq, true), {
+const mBotGetDistance = _.merge(_.clone(OutputBloq, true), {
 
-    name: 'mBotGetDistance-v2',
-    bloqClass: 'bloq-mbot-getdistance-v2',
-    content: [
-        [{
-            alias: 'text',
-            value: 'bloq-mbot-getdistance'
-        }, {
-            id: 'ULTRASOUND',
-            alias: 'dynamicDropdown',
-            options: 'mkb_ultrasound'
-        }, {
-            alias: 'text',
-            value: 'in'
-        }, {
-            id: 'MAGNITUDE',
-            alias: 'staticDropdown',
-            options: [{
-                label: 'cm',
-                value: 'cm'
-            }, {
-                label: 'inches',
-                value: 'inches'
-            }]
-        }]
-    ],
-    code: '',
-    returnType: {
-        type: 'simple',
-        value: 'float'
+  name: 'mBotGetDistance-v2',
+  bloqClass: 'bloq-mbot-getdistance-v2',
+  content: [
+    [{
+      alias: 'text',
+      value: 'bloq-mbot-getdistance',
+    }, {
+      id: 'ULTRASOUND',
+      alias: 'dynamicDropdown',
+      options: 'mkb_ultrasound',
+    }, {
+      alias: 'text',
+      value: 'in',
+    }, {
+      id: 'MAGNITUDE',
+      alias: 'staticDropdown',
+      options: [{
+        label: 'cm',
+        value: 'cm',
+      }, {
+        label: 'inches',
+        value: 'inches',
+      }],
+    }],
+  ],
+  code: '',
+  returnType: {
+    type: 'simple',
+    value: 'float',
+  },
+  arduino: {
+    conditional: {
+      aliasId: 'MAGNITUDE',
+      code: {
+        cm: '{ULTRASOUND}.readDistanceInCM()',
+        inches: '{ULTRASOUND}.readDistanceInInches()',
+      },
     },
-    arduino: {
-        conditional: {
-            aliasId: 'MAGNITUDE',
-            code: {
-                'cm': '{ULTRASOUND}.readDistanceInCM()',
-                'inches': '{ULTRASOUND}.readDistanceInInches()'
-            }
-        }
-    }
+  },
 });
 utils.preprocessBloq(mBotGetDistance);
 
--- src/scripts/bloqs/zumjunior/zumjuniorButtonIf.js
+++ src/scripts/bloqs/zumjunior/zumjuniorButtonIf.js
@@ -1,9 +1,10 @@
-/*global require */
-'use strict';
+/* global require */
 
-var _ = require('lodash'),
-    utils = require('./../build-utils'),
-    StatementInputBloq = require('./../statementInputBloq');
+
+
+let _ = require('lodash'),
+  utils = require('./../build-utils'),
+  StatementInputBloq = require('./../statementInputBloq');
 
 /**
  * Bloq name: zumjuniorButtonIf
@@ -15,41 +16,41 @@ var _ = require('lodash'),
  * Return type: none
  */
 
-var zumjuniorButtonIf = _.merge(_.clone(StatementInputBloq, true), {
+let zumjuniorButtonIf = _.merge(_.clone(StatementInputBloq, true), {
 
-    name: 'zumjuniorButtonIf',
-    bloqClass: 'bloq-zumjunior-button-if',
-    content: [
-        [{
-            alias: 'text',
-            value: 'bloq-zumjunior-button-if'
-        }, {
-            id: 'BUTTON',
-            alias: 'dynamicDropdown',
-            options: 'zumjuniorButtons'
-        }, {
-            alias: 'text',
-            value: 'bloq-zumjunior-button-is'
-        }, {
-            id: 'IS_PRESSED',
-            alias: 'staticDropdown',
-            options: [{
-                label: 'bloq-zumjunior-button-pressed',
-                value: 'digitalRead'
-            }, {
-                label: 'bloq-zumjunior-button-notpressed',
-                value: '!digitalRead'
-            }]
-        }, {
-            alias: 'text',
-            value: 'bloq-zumjunior-button-exec'
-        }]
-    ],
-    suggestedBloqs: ['else'],
+  name: 'zumjuniorButtonIf',
+  bloqClass: 'bloq-zumjunior-button-if',
+  content: [
+    [{
+      alias: 'text',
+      value: 'bloq-zumjunior-button-if',
+    }, {
+      id: 'BUTTON',
+      alias: 'dynamicDropdown',
+      options: 'zumjuniorButtons',
+    }, {
+      alias: 'text',
+      value: 'bloq-zumjunior-button-is',
+    }, {
+      id: 'IS_PRESSED',
+      alias: 'staticDropdown',
+      options: [{
+        label: 'bloq-zumjunior-button-pressed',
+        value: 'digitalRead',
+      }, {
+        label: 'bloq-zumjunior-button-notpressed',
+        value: '!digitalRead',
+      }],
+    }, {
+      alias: 'text',
+      value: 'bloq-zumjunior-button-exec',
+    }],
+  ],
+  suggestedBloqs: ['else'],
+  code: 'if({IS_PRESSED}({BUTTON}Pin)){{STATEMENTS}}',
+  arduino: {
     code: 'if({IS_PRESSED}({BUTTON}Pin)){{STATEMENTS}}',
-    arduino: {
-        code: 'if({IS_PRESSED}({BUTTON}Pin)){{STATEMENTS}}',
-    }
+  },
 });
 
 utils.preprocessBloq(zumjuniorButtonIf);
